5be9b200b4c2ea643f7af3aa666566d35e1b62c6
rodolforomera <rodolforomeras@gmail.com>
2021-06-22 22:16:21 -0300

    Frontend: Componente todoForm

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -6,11 +6,14 @@ export default props => (
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
             <input id='description' className='form-control'
-                placeholder='Adicione uma tarefa'>
+                placeholder='Adicione uma tarefa'
+                value={props.description}
+                onChange={props.handleChange}>
             </input>
         </Grid>
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'>
+            <IconButton style='primary' icon='plus'
+                onClick={props.handleAdd}>
             </IconButton>
         </Grid>
     </div>
