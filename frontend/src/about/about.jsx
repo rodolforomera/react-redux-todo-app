7834ba371a19ae22068e0e448dc3526ebfc4b6a9
rodolforomera <rodolforomeras@gmail.com>
2021-06-22 21:27:29 -0300

    Frontend: Criação do menu e configuração das rotas; Comoponentes App, Todo e About

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        
+        <h3>Nossa História</h3>
+        <p>............................</p>
+        <h3>Missão e Visão</h3>
+        <p>...........................</p>
+        <h3>Imprensa</h3>
+        <p>...........................</p>
+    </div>
+)
